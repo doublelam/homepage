6da6d96d7dd5bc6148f4545b16c2e1dc886f3154
doublelam <1422015387@qq.com>
2015-07-09 22:09:46 +0800

    新的进展，new elements added
    由原来的只有左上角送至北京写到手机京东

--- programs/仿京东/js/script.js
+++ programs/仿京东/js/script.js
@@ -1,16 +1,21 @@
-var oUl=document.getElementById('address');
+
+
+
+/*大的function对鼠标滑入产生隐藏显现的div块进行参数化，构造可以通用的method*/
+function onmouseover_hidden(overid,rotateid,hiddenableid){
+
+
+
+var oUl=document.getElementById(hiddenableid);
 var aLi=oUl.getElementsByTagName('li');
-var oExp=document.getElementById('express');
-var oUpd=document.getElementById('upordown');
+var oExp=document.getElementById(overid);
+var oUpd=document.getElementById(rotateid);
 var oP=document.getElementById('addres');
-for(var i=0;i<aLi.length;i++){
-	aLi[i].style.top=38*(i%6)+10+"px";
-	aLi[i].style.left=75*(parseInt(i/6))+25+"px";
-};
 
 
 
 
+//"送至：..."这个div鼠标滑进去的变化
 function exactive(){
 	oExp.style.background="white";
 	oExp.style.borderRight="1px solid #DDDDDD";
@@ -20,8 +25,10 @@ function exactive(){
 	oUpd.style.transformOrigin="40% 80%";
 	//oExp.style.background="blue";
 };
+///////////////////////////////////////////////
 
 
+// "送至：..."这个div鼠标移出来的变化
 function unexactive(){
 
 	oExp.style.background="#F1F1F1";
@@ -32,17 +39,43 @@ function unexactive(){
 	oUpd.style.transformOrigin="40% 80%";
 	//oExp.style.background="blue";	
 }
+//////////////////////////////////////////////
+
+//"送至：..."的div在移出时消失的效果
+oUl.onmouseout=function(){
+	unexactive();
+}
+//跟随的块鼠标移出时的效果
+oExp.onmouseout=function(){
+	unexactive();
+}
+///////////////////////////////////////
+//"送至：..."的div在移出时消失的效果
+oUl.onmouseover=function(){
+	exactive();
+}
+//跟随的块鼠标移出时的效果
+oExp.onmouseover=function(){
+	exactive();
+}
+//////////////////////////////////////////
 
 
+//地名的颜色效果
+if (aLi[0]!==undefined&&arguments[3]!=="linotfollow") {
 
 
-
-
+//全国地名在div中的平均分布
+for(var i=0;i<aLi.length;i++){
+	aLi[i].style.top=38*(i%6)+10+"px";
+	aLi[i].style.left=75*(parseInt(i/6))+25+"px";
+};
+//////////////////////////////////////////////////	
 aLi[0].className="addreclicked";
 
 for(var i=0;i<aLi.length;i++){
 
-	//单击地名改变背景和字体颜色以及地址信息
+//单击地名改变背景和字体颜色以及地址信息
 	aLi[i].onclick=function(){
   for (var j =0; j<aLi.length;j++) {
   	aLi[j].className="";
@@ -52,7 +85,8 @@ for(var i=0;i<aLi.length;i++){
     oP.innerHTML="送至："+this.innerHTML;
     }
 
-  
+
+////////////////////////////////////////////  
 
 
 //地名onmouseover效果
@@ -63,31 +97,31 @@ this.className="addreover";
 
 }
 
-//划出时消失
+//地名list划出时消失
 aLi[i].onmouseout=function(){
 	if(this.className=="addreover")
 {this.className="";}
 };
 
-
-	}
-
-
-
-
-
-oUl.onmouseout=function(){
-	unexactive();
+ };
+	
 }
-oExp.onmouseout=function(){
-	unexactive();
+//我的京东下面的list排布
+else if(arguments[3]==="linotfollow"&&arguments[4]==="listofmyjingdong"){
+for(var i=0;i<aLi.length;i++){
+	aLi[i].style.top=32*(i%4)+60+"px";
+	aLi[i].style.left=100*(parseInt(i/4))+25+"px";
+};
+////////////////////////////////////////////////////////////////
+}
+	
 }
 
 
-oUl.onmouseover=function(){
-	exactive();
-}
-oExp.onmouseover=function(){
-	exactive();
-}
+onmouseover_hidden("express","upordown","address");
+
+onmouseover_hidden("myjingdong","myjingdongdiv","minejingdong","linotfollow","listofmyjingdong");
+
+onmouseover_hidden("phonejingdong","fonerotatediv","fonejingdongdiv");
+
 
